63ce14d9471827155a893d72e5e270dbef86b311
kristapspriede <kristaps.priede@gmail.com>
2019-10-16 00:13:14 +0300

    added flight-planner db

---
+++ flight-planner-db/flight-planner-db-mapper/flight-planner.services/FlightService.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using flight_planner.core.Models;
+using flight_planner.core.Services;
+using flight_planner.data;
+
+
+namespace flight_planner.services
+{
+    public class FlightService
+    {
+        private static readonly Object obj = new Object(); 
+        public async Task<ICollection<Flight>> GetFlights()
+        {
+            using (var context = new FlightPlannerDbContext())
+            {
+                return await context.Flights.Include(f => f.To).Include(f => f.From).ToListAsync();
+            }
+        }
+
+        public async Task<ICollection<Airport>> GetAirports()
+        {
+            using (var context = new FlightPlannerDbContext())
+            {
+                return await context.Airports.ToListAsync();
+            }
+        }
+
+
+        public async Task<Flight> AddFlight(Flight flight)
+        {
+            using (var context = new FlightPlannerDbContext())
+            {
+                
+                    if (!Exists(flight))
+                    {
+                        flight = context.Flights.Add(flight);
+                        await context.SaveChangesAsync();
+                    }
+
+                    return flight;
+            }
+        }
+
+        public async Task<Flight> GetFlightById(int id)
+        {
+            using (var context = new FlightPlannerDbContext())
+            {
+                var flight = await context.Flights.Include(f => f.To).Include(f => f.From).SingleOrDefaultAsync(f => f.Id == id);
+                    return flight;
+            }
+        }
+        public async Task DeleteFlight(Flight flight)
+        {
+            using (var context = new FlightPlannerDbContext())
+            {
+                context.Flights.Attach(flight);
+                    context.Flights.Remove(flight);
+                    await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<ICollection<Airport>> SearchAirports(string search)
+        {
+            search = search.ToLowerInvariant().Trim();
+            using (var context = new FlightPlannerDbContext())
+            {
+                var query = context.Airports.Where(a => a.City.ToLower().Contains(search.ToLower())
+                                                        && a.Country.ToLower().Contains(search.ToLower())
+                                                        && a.AirportCode.ToLower().Contains(search.ToLower()));
+                return await query.ToListAsync();
+            }
+        }
+        public async Task<bool> DeleteFlightById(int id)
+        {
+           
+                using (var context = new FlightPlannerDbContext())
+                {
+                    var flight = await context.Flights.SingleOrDefaultAsync(f => f.Id == id);
+                if (flight != null)
+                {
+                    context.Flights.Remove(flight);
+                    await context.SaveChangesAsync();
+                }
+
+                return true;
+                }
+            
+        }
+        public static bool Exists(Flight flight)
+        {
+            using (var context = new FlightPlannerDbContext())
+            {
+                return context.Flights.Any(f =>
+                        //f.Id == flight.Id &&
+                        f.From.AirportCode == flight.From.AirportCode &&
+                        f.From.City == flight.From.City &&
+                        f.From.Country == flight.From.Country &&
+                        f.To.AirportCode == flight.To.AirportCode &&
+                        f.To.City == flight.To.City &&
+                        f.To.Country == flight.To.Country &&
+                        f.Carrier == flight.Carrier &&
+                        f.ArrivalTime == flight.ArrivalTime &&
+                        f.DepartureTime == flight.DepartureTime);
+
+            }
+        }
+        public static async Task<bool> DeleteFlights()
+        {
+            using (var context = new FlightPlannerDbContext())
+            {
+                context.Flights.RemoveRange(context.Flights);
+                context.Airports.RemoveRange(context.Airports);
+                await context.SaveChangesAsync();
+                return true;
+            }
+        }
+    }
+}
---
+++ flight-planner-db/flight-planner-db-mapper/flight-planner/Controllers/CustomerApiController.cs
@@ -0,0 +1,145 @@
+﻿using flight_planner.Models;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using flight_planner.core.Models;
+using flight_planner.services;
+using flight_planner.Controllers;
+
+
+
+
+namespace flight_planner.Controllers
+{
+    public class CustomerApiController : ApiController
+    {
+        private readonly FlightService _flightService;
+        public CustomerApiController()
+        {
+            _flightService = new FlightService();
+
+        }
+        [HttpGet]
+        [Route("api/FlightSearchRequest/{id}")]
+        
+        public async Task<IHttpActionResult> Get(int id)
+        {
+            var flight = await _flightService.GetFlightById(id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            return Ok(convertFlightToFlightRequest(flight));
+        }
+
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        // GET: api/CustomerApi/5
+
+        [HttpGet]
+        [Route("api/airports")]
+        public async Task<IHttpActionResult> GetAirports(string search)
+        {
+            var airports = await _flightService.GetAirports();//SearchAirports
+            var result = new HashSet<AirportRequest>();
+            airports.ToList().ForEach(a =>
+            {
+                result.Add(ConvertAirportToAirportRequest(a));
+
+            });
+            return Ok(result
+                .Where(a => a.Airport.ToLower().Contains(search.ToLower().Trim()) ||
+                            a.City.ToLower().Contains(search.ToLower().Trim()) ||
+                            a.Country.ToLower().Contains(search.ToLower().Trim()))
+                .ToArray());
+        }
+        
+
+        // POST: api/CustomerApi
+        [HttpPost]
+        [Route("api/flights/search")]
+        public async Task<HttpResponseMessage> FlightSearch(HttpRequestMessage request, FlightSearchRequest search)
+        {
+            if (IsValid(search) && NotSameAirport(search))
+            {
+                var result = await _flightService.GetFlights();
+                var matchedItems = result.DistinctBy(f => f.From.AirportCode.ToLower().Contains(search.From.ToLower()) ||
+                                                     f.To.AirportCode.ToLower().Contains(search.To.ToLower()) ||
+                                                     DateTime.Parse(f.DepartureTime) ==
+                                                     DateTime.Parse(search.DepartureDate)).ToList();
+                var response = new FlightSearchResult
+                {
+                    TotalItems = matchedItems.Count,
+                    Items = matchedItems,
+                    Page = matchedItems.Any() ? 1 : 0
+                };
+                return request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            else
+            {
+                return request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+
+        private bool NotSameAirport(FlightSearchRequest search)
+        {
+            return !string.Equals(search.From, search.To, StringComparison.InvariantCultureIgnoreCase);
+        }
+        private bool IsValid(FlightSearchRequest search)
+        {
+            return search != null && !string.IsNullOrEmpty(search.From) &&
+                                     !string.IsNullOrEmpty(search.To) &&
+                                     !string.IsNullOrEmpty(search.DepartureDate);
+        }
+
+        [HttpGet]
+        [Route("api/flights/{id}")]
+        public async Task<IHttpActionResult> FlightSearchById(int id)
+        {
+            var flight = await _flightService.GetFlightById(id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            return Ok(convertFlightToFlightRequest(flight));
+        }
+        // PUT: api/CustomerApi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        // DELETE: api/CustomerApi/5
+        public void Delete(int id)
+        {
+        }
+        protected FlightRequest convertFlightToFlightRequest(Flight flight)
+        {
+            return new FlightRequest
+            {
+                Id = flight.Id,
+                To = ConvertAirportToAirportRequest(flight.To),
+                From = ConvertAirportToAirportRequest(flight.From),
+                Carrier = flight.Carrier,
+                ArrivalTime = flight.ArrivalTime,
+                DepartureTime = flight.DepartureTime
+            };
+        }
+
+        private AirportRequest ConvertAirportToAirportRequest(Airport airport)
+        {
+            return new AirportRequest
+            {
+                City = airport.City,
+                Country = airport.Country,
+                Airport = airport.AirportCode
+            };
+        }
+    }
+}
